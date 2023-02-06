255e1948a2597e4ad31309d90f021eb9424f0eef
juliop11 <julioperezobrero@hotmail.com>
2023-02-06 14:01:00 +0100

    odificar funcion

---
+++ proyecto1.js
@@ -0,0 +1,43 @@
+
+function calculadoraSuma(op1, op2) {
+    let sum;
+
+    sum = op1 + op2;
+    console.log(sum);
+
+    return sum;
+}
+
+function calculadoraResta(op1, op2) {
+    let res;
+
+    res = op1 - op2;
+    console.log(res);
+
+    return res;
+}
+
+function calculadoraMulti(op1, op2) {
+    let multi;
+
+    multi = op1 * op2;
+    console.log(multi);
+
+    return multi;
+}
+
+function calculadoraDivision(op1, op2) {
+    let div;
+
+    div = op1 / op2;
+    console.log(div);
+
+    return div;
+}
+
+function cuadrado(num1) {
+    let num;
+    num = num1 * num1;
+    console.log(num);
+}
+cuadrado(4);
